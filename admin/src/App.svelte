ecd1f86e0d3c84ba26956d9fe971077a2b784210
Kasper Ingelvide <kasper@hubben.xyz>
2024-05-20 22:23:24 +0200

    Add admin stub

---
+++ admin/src/App.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import "@shoelace-style/shoelace/dist/themes/light.css";
+  import { setBasePath } from "@shoelace-style/shoelace/dist/utilities/base-path.js";
+  import { QueryClient, QueryClientProvider } from "@tanstack/svelte-query";
+  import { SvelteQueryDevtools } from "@tanstack/svelte-query-devtools";
+  import { Route, Router } from "svelte-routing";
+  import "./main.css";
+  import Start from "./pages/Start.svelte";
+
+  setBasePath("/shoelace");
+
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+      },
+    },
+  });
+</script>
+
+<QueryClientProvider client={queryClient}>
+  <Router>
+    <Route path="/"><Start /></Route>
+  </Router>
+  {#if import.meta.env.DEV && false}
+    <SvelteQueryDevtools />
+  {/if}
+</QueryClientProvider>
---
+++ admin/vite.config.ts
@@ -0,0 +1,25 @@
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import path from "path";
+import copy from "rollup-plugin-copy";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  build: {
+    minify: true,
+  },
+  plugins: [
+    svelte(),
+    copy({
+      copyOnce: true,
+      targets: [
+        {
+          src: path.resolve(
+            __dirname,
+            "node_modules/@shoelace-style/shoelace/dist/assets"
+          ),
+          dest: path.resolve(__dirname, "public/shoelace"),
+        },
+      ],
+    }),
+  ],
+});
