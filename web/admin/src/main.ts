16bf92dca0770eedcb5fd6f3e22177f89590e5fe
Kasper Ingelvide <7457770+kasper-i@users.noreply.github.com>
2024-05-29 19:40:33 +0200

    fix: wrong css import order (#24)

--- web/admin/src/main.ts
+++ web/admin/src/main.ts
@@ -1,8 +1,5 @@
 import App from "@/App.svelte";
 import "@/main.css";
-import "@climblive/lib/theme.css";
-import "@shoelace-style/shoelace/dist/themes/light.css";
-import "normalize.css/normalize.css";
 
 const app = new App({
   target: document.body,
