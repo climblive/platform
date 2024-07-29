b0b9abe468a51ea2bc40a3f14061eccf03af2522
Kasper Ingelvide <kasper@hubben.xyz>
2024-07-29 13:48:53 +0200

    Add some samples

---
+++ backend/database/samples.sql
@@ -0,0 +1,5 @@
+INSERT INTO organizer VALUES (NULL, 'Test Organizer', 'https://example.org');
+INSERT INTO series VALUES (NULL, 1, 'Test series');
+INSERT INTO contest VALUES (NULL, 1, FALSE, 1, 'World Testing Championships', 'The worlds number one competition for testing', 'On the web', TRUE, 10, 7, 'Everything is allowed', 300);
+INSERT INTO comp_class VALUES (NULL, 1, 1, 'Unicorns', '16 years and older', '#ff0000', '2024-01-01 00:00:00', '2024-01-01 06:00:00');
+INSERT INTO contender VALUES (NULL, 1, 1, 'ABCD1234', 'Albert Einstein', 'Testers\' Climbing Club', 1, '2024-01-01 00:00:00', FALSE);
