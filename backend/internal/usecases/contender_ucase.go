fdb268791965be69a7b2fd42dbdcde3d84ab9013
Kasper Ingelvide <kasper@hubben.xyz>
2024-08-17 11:16:00 +0200

    Fix role check

--- backend/internal/usecases/contender_ucase.go
+++ backend/internal/usecases/contender_ucase.go
@@ -236,7 +236,7 @@ func (uc *ContenderUseCase) DeleteContender(ctx context.Context, contenderID dom
 		return err
 	}
 
-	if role != "" && !role.OneOf(domain.AdminRole, domain.OrganizerRole) {
+	if !role.OneOf(domain.AdminRole, domain.OrganizerRole) {
 		return domain.ErrNotAllowed
 	}
 
