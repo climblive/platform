3918d71adff10c46662caf1a5b877789f8c96de3
Kasper Ingelvide <kasper@hubben.xyz>
2024-09-17 20:53:08 +0200

    feat: add create tick endpoint

--- backend/internal/usecases/tick_ucase.go
+++ backend/internal/usecases/tick_ucase.go
@@ -2,6 +2,7 @@ package usecases
 
 import (
 	"context"
+	"time"
 
 	"github.com/climblive/platform/backend/internal/domain"
 	"github.com/go-errors/errors"
@@ -12,11 +13,17 @@ type tickUseCaseRepository interface {
 
 	GetContender(ctx context.Context, tx domain.Transaction, contenderID domain.ResourceID) (domain.Contender, error)
 	GetTicksByContender(ctx context.Context, tx domain.Transaction, contenderID domain.ResourceID) ([]domain.Tick, error)
+	GetContest(ctx context.Context, tx domain.Transaction, contestID domain.ResourceID) (domain.Contest, error)
+	GetCompClass(ctx context.Context, tx domain.Transaction, compClassID domain.ResourceID) (domain.CompClass, error)
+	GetProblem(ctx context.Context, tx domain.Transaction, problemID domain.ResourceID) (domain.Problem, error)
+	DeleteTick(ctx context.Context, tx domain.Transaction, tickID domain.ResourceID) error
+	CreateTick(ctx context.Context, tx domain.Transaction, tick domain.Tick) (domain.Tick, error)
 }
 
 type TickUseCase struct {
-	Repo       tickUseCaseRepository
-	Authorizer domain.Authorizer
+	Repo        tickUseCaseRepository
+	Authorizer  domain.Authorizer
+	EventBroker domain.EventBroker
 }
 
 func (uc *TickUseCase) GetTicksByContender(ctx context.Context, contenderID domain.ResourceID) ([]domain.Tick, error) {
@@ -46,5 +53,63 @@ func (uc *TickUseCase) DeleteTick(ctx context.Context, tickID domain.ResourceID)
 }
 
 func (uc *TickUseCase) CreateTick(ctx context.Context, contenderID domain.ResourceID, tick domain.Tick) (domain.Tick, error) {
-	panic("not implemented")
+	contender, err := uc.Repo.GetContender(ctx, nil, contenderID)
+	if err != nil {
+		return domain.Tick{}, errors.Wrap(err, 0)
+	}
+
+	role, err := uc.Authorizer.HasOwnership(ctx, contender.Ownership)
+	if err != nil {
+		return domain.Tick{}, errors.Wrap(err, 0)
+	}
+
+	problem, err := uc.Repo.GetProblem(ctx, nil, tick.ProblemID)
+	if err != nil {
+		return domain.Tick{}, errors.Wrap(err, 0)
+	}
+
+	contest, err := uc.Repo.GetContest(ctx, nil, contender.ContestID)
+	if err != nil {
+		return domain.Tick{}, errors.Errorf("%w: %w", domain.ErrRepositoryIntegrityViolation, err)
+	}
+
+	compClass, err := uc.Repo.GetCompClass(ctx, nil, contender.CompClassID)
+	if err != nil {
+		return domain.Tick{}, errors.Errorf("%w: %w", domain.ErrRepositoryIntegrityViolation, err)
+	}
+
+	gracePeriodEnd := compClass.TimeEnd.Add(contest.GracePeriod)
+
+	switch {
+	case role.OneOf(domain.OrganizerRole, domain.AdminRole):
+	case time.Now().After(gracePeriodEnd):
+		return domain.Tick{}, errors.New(domain.ErrContestEnded)
+	}
+
+	newTick := domain.Tick{
+		Ownership:    contender.Ownership,
+		Timestamp:    time.Now(),
+		ContestID:    contest.ID,
+		ProblemID:    problem.ID,
+		Top:          tick.Top,
+		AttemptsTop:  tick.AttemptsTop,
+		Zone:         tick.Zone,
+		AttemptsZone: tick.AttemptsZone,
+	}
+
+	tick, err = uc.Repo.CreateTick(ctx, nil, newTick)
+	if err != nil {
+		return domain.Tick{}, errors.Wrap(err, 0)
+	}
+
+	uc.EventBroker.Dispatch(contest.ID, domain.AscentRegisteredEvent{
+		ContenderID:  contender.ID,
+		ProblemID:    problem.ID,
+		Top:          tick.Top,
+		AttemptsTop:  tick.AttemptsTop,
+		Zone:         tick.Zone,
+		AttemptsZone: tick.AttemptsTop,
+	})
+
+	return tick, nil
 }
