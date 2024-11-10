618453ead16bdf5f3b49117ce23cdab78cd35e46
Kasper Ingelvide <kasper@hubben.xyz>
2024-11-10 14:53:02 +0100

    feat: add scoreboard mobile view

--- web/scoreboard/src/pages/Scoreboard.svelte
+++ web/scoreboard/src/pages/Scoreboard.svelte
@@ -2,15 +2,28 @@
   import Header from "@/components/Header.svelte";
   import { ResultList, ScoreboardProvider } from "@climblive/lib/components";
   import { getCompClassesQuery, getContestQuery } from "@climblive/lib/queries";
+  import "@shoelace-style/shoelace/dist/components/select/select.js";
   import Loading from "./Loading.svelte";
 
   export let contestId: number;
+
+  let selectedCompClassId: number | undefined = 1;
 
   const contestQuery = getContestQuery(contestId);
   const compClassesQuery = getCompClassesQuery(contestId);
 
   $: contest = $contestQuery.data;
   $: compClasses = $compClassesQuery.data;
+
+  const value = (node: HTMLElement, value: string | number | undefined) => {
+    node.setAttribute("value", value?.toString() ?? "");
+
+    return {
+      update(value: string | number | undefined) {
+        node.setAttribute("value", value?.toString() ?? "");
+      },
+    };
+  };
 </script>
 
 {#if !contest || !compClasses}
@@ -19,12 +32,24 @@
   <ScoreboardProvider {contestId}>
     <main>
       <h1>{contest.name}</h1>
-      <div
-        class="container"
-        style="grid-template-columns: repeat({compClasses.length}, 1fr)"
+      <sl-select
+        size="small"
+        name="compClassId"
+        label="Competition class"
+        use:value={selectedCompClassId}
       >
         {#each compClasses as compClass}
-          <section class="class">
+          <sl-option value={compClass.id}>{compClass.name}</sl-option>
+        {/each}
+      </sl-select>
+      <div class="container" style="--num-columns: {compClasses.length}">
+        {#each compClasses as compClass}
+          <section
+            class="class"
+            data-selected={compClass.id === selectedCompClassId
+              ? "true"
+              : "false"}
+          >
             <Header
               compClassId={compClass.id}
               name={compClass.name}
@@ -44,6 +69,7 @@
     display: flex;
     flex-direction: column;
     height: 100%;
+    padding: var(--sl-spacing-small);
   }
 
   h1 {
@@ -54,8 +80,8 @@
   .container {
     flex-grow: 1;
     display: grid;
+    grid-template-columns: repeat(var(--num-columns), 1fr);
     grid-template-rows: 1fr;
-    padding: var(--sl-spacing-small);
     padding-top: 0;
     gap: var(--sl-spacing-small);
   }
@@ -63,5 +89,23 @@
   .class {
     display: flex;
     flex-direction: column;
+  }
+
+  sl-select {
+    display: none;
+  }
+
+  @media screen and (max-width: 32rem) {
+    sl-select {
+      display: block;
+    }
+
+    .container {
+      grid-template-columns: 1fr;
+    }
+
+    .class[data-selected="false"] {
+      display: none;
+    }
   }
 </style>
