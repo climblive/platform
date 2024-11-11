0ea33fbb1de425923b41bc8a44f5c7aa0cd19037
Kasper Ingelvide <7457770+kasper-i@users.noreply.github.com>
2024-11-11 17:27:15 +0000

    feat: configure api url automatically in codespace

--- .devcontainer/scripts/post.sh
+++ .devcontainer/scripts/post.sh
@@ -5,4 +5,9 @@ sudo mariadb -e "CREATE DATABASE climblive;"
 sudo mariadb -e "CREATE USER climblive@localhost IDENTIFIED BY 'secretpassword';"
 sudo mariadb -e "GRANT ALL PRIVILEGES ON climblive.* TO climblive@localhost;"
 sudo mariadb climblive -e "SOURCE backend/database/scoreboard.sql" --default-character-set utf8mb4
-sudo mariadb climblive -e "SOURCE backend/database/samples.sql" --default-character-set utf8mb4
+sudo mariadb climblive -e "SOURCE backend/database/samples.sql" --default-character-set utf8mb4o
+
+if [[ -n "${CODESPACE_NAME}" ]]; then
+    API_URL="https://${CODESPACE_NAME}-8090.app.github.dev"
+    sed -i "s,\"API_URL\":.*,\"API_URL\": \"${API_URL}\",g" web/packages/lib/src/config.json
+fi
