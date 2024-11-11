ad320fe9de0478a19f8a96e00b6ee64bc300c916
Kasper Ingelvide <7457770+kasper-i@users.noreply.github.com>
2024-11-11 18:20:37 +0000

    Fixes

--- .devcontainer/scripts/post.sh
+++ .devcontainer/scripts/post.sh
@@ -5,7 +5,7 @@ sudo mariadb -e "CREATE DATABASE climblive;"
 sudo mariadb -e "CREATE USER climblive@localhost IDENTIFIED BY 'secretpassword';"
 sudo mariadb -e "GRANT ALL PRIVILEGES ON climblive.* TO climblive@localhost;"
 sudo mariadb climblive -e "SOURCE backend/database/scoreboard.sql" --default-character-set utf8mb4
-sudo mariadb climblive -e "SOURCE backend/database/samples.sql" --default-character-set utf8mb4o
+sudo mariadb climblive -e "SOURCE backend/database/samples.sql" --default-character-set utf8mb4
 
 if [[ -n "${CODESPACE_NAME}" ]]; then
     API_URL="https://${CODESPACE_NAME}-8090.app.github.dev"
