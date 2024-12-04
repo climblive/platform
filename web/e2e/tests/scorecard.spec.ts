b95ead11ad487063473673204b44a2c69909cf32
Kasper Ingelvide <7457770+kasper-i@users.noreply.github.com>
2024-12-04 21:20:48 +0100

    feat: show rules in scorecard (#170)

--- web/e2e/tests/scorecard.spec.ts
+++ web/e2e/tests/scorecard.spec.ts
@@ -226,6 +226,14 @@ test("flash a problem", async ({ page }) => {
   await expect(problem.getByText("+110p")).not.toBeVisible();
 })
 
+test("show rules", async ({ page }) => {
+  await page.goto('/ABCD0001');
+
+  await page.getByRole("tab", { name: "Rules" }).click();
+
+  await expect(page.getByText("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")).toBeVisible();
+})
+
 test.describe("contest states", () => {
   test.beforeEach(async ({ page }) => {
     await page.clock.setFixedTime(new Date('2023-11-01T00:00:00'));
