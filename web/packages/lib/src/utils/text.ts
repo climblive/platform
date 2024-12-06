9d6d93efa7408656597ea653614eee42f89b2249
Kasper Ingelvide <kasper@hubben.xyz>
2024-12-07 00:01:15 +0100

    fix: ordinals not shown correctly

--- web/packages/lib/src/utils/text.ts
+++ web/packages/lib/src/utils/text.ts
@@ -1,11 +1,19 @@
 export const asOrdinal = (value: number): string => {
-  switch (value) {
-    case 1:
+  switch (true) {
+    case value === 1:
       return "1st";
-    case 2:
+    case value === 2:
       return "2nd";
-    case 3:
+    case value === 3:
       return "3rd";
+    case value <= 20:
+      return `${value}th`;
+    case value % 10 === 1:
+      return `${value}st`;
+    case value % 10 === 2:
+      return `${value}nd`;
+    case value % 10 === 3:
+      return `${value}rd`;
     default:
       return `${value}th`;
   }
