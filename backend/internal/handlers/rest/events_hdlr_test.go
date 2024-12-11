5b6681cbc47b22d19e7ea83488b5ebbe25af8f83
Kasper Ingelvide <kasper@hubben.xyz>
2024-12-11 21:41:49 +0100

    Add connect and disconnect test

---
+++ backend/internal/handlers/rest/events_hdlr_test.go
@@ -0,0 +1,82 @@
+package rest_test
+
+import (
+	"bufio"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/climblive/platform/backend/internal/domain"
+	"github.com/climblive/platform/backend/internal/events"
+	"github.com/climblive/platform/backend/internal/handlers/rest"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+)
+
+func TestEventsHandler(t *testing.T) {
+	makeMocks := func(bufferCapacity int) (*eventBrokerMock, *events.Subscription) {
+		mockedEventBroker := new(eventBrokerMock)
+
+		subscription := events.NewSubscription(domain.EventFilter{}, bufferCapacity)
+		subscriptionID := uuid.New()
+
+		mockedEventBroker.On("Subscribe", domain.NewEventFilter(
+			0,
+			domain.ContenderID(1),
+			"CONTENDER_PUBLIC_INFO_UPDATED",
+			"CONTENDER_SCORE_UPDATED",
+		), 1000).Return(subscriptionID, subscription)
+
+		mockedEventBroker.On("Unsubscribe", subscriptionID).Return()
+
+		return mockedEventBroker, subscription
+	}
+
+	t.Run("ConnectAndDisconnect", func(t *testing.T) {
+		mockedEventBroker, _ := makeMocks(0)
+
+		mux := rest.NewMux()
+		rest.InstallEventHandler(mux, mockedEventBroker)
+
+		server := httptest.NewServer(mux)
+
+		resp, err := http.Get(server.URL + "/contenders/1/events")
+		require.NoError(t, err)
+
+		assert.Equal(t, http.StatusOK, resp.StatusCode)
+		assert.Equal(t, "text/event-stream", resp.Header.Get("Content-Type"))
+		assert.Equal(t, "no", resp.Header.Get("X-Accel-Buffering"))
+		assert.Equal(t, "no-cache", resp.Header.Get("Cache-Control"))
+		assert.Equal(t, "keep-alive", resp.Header.Get("Connection"))
+
+		buf := bufio.NewReader(resp.Body)
+		line, _, err := buf.ReadLine()
+		require.NoError(t, err)
+		assert.Equal(t, "retry: 5000", string(line))
+
+		resp.Body.Close()
+
+		server.Close()
+
+		mockedEventBroker.AssertExpectations(t)
+	})
+}
+
+type eventBrokerMock struct {
+	mock.Mock
+}
+
+func (m *eventBrokerMock) Dispatch(contestID domain.ContestID, event any) {
+	m.Called(contestID, event)
+}
+
+func (m *eventBrokerMock) Subscribe(filter domain.EventFilter, bufferCapacity int) (domain.SubscriptionID, domain.EventReader) {
+	args := m.Called(filter, bufferCapacity)
+	return args.Get(0).(domain.SubscriptionID), args.Get(1).(domain.EventReader)
+}
+
+func (m *eventBrokerMock) Unsubscribe(subscriptionID domain.SubscriptionID) {
+	m.Called(subscriptionID)
+}
