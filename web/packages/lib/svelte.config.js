1acec5fc04fd0b26c3f928dbdc816c510791b3ae
Kasper Ingelvide <kasper@hubben.xyz>
2024-12-14 16:02:08 +0100

    Format

--- web/packages/lib/svelte.config.js
+++ web/packages/lib/svelte.config.js
@@ -5,11 +5,11 @@ export default {
   // for more information about preprocessors
   preprocess: vitePreprocess(),
   compilerOptions: {
-    warningFilter: (warning) => !ignoreWarning[warning.code]?.(warning)
+    warningFilter: (warning) => !ignoreWarning[warning.code]?.(warning),
   },
 };
 
 const ignoreWarning = {
-  "a11y_no_static_element_interactions": (w) => w.message.startsWith("`<sl-"),
-  "a11y_click_events_have_key_events": () => true
-}
+  a11y_no_static_element_interactions: (w) => w.message.startsWith("`<sl-"),
+  a11y_click_events_have_key_events: () => true,
+};
