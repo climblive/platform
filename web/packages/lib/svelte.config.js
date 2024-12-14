cd607783fd6b9c0702e9f10072f35f973a7c4bcc
Kasper Ingelvide <7457770+kasper-i@users.noreply.github.com>
2024-12-14 16:05:41 +0100

    fix: disable annoying svelte warnings (#197)

--- web/packages/lib/svelte.config.js
+++ web/packages/lib/svelte.config.js
@@ -4,4 +4,12 @@ export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
   preprocess: vitePreprocess(),
+  compilerOptions: {
+    warningFilter: (warning) => !ignoreWarning[warning.code]?.(warning),
+  },
+};
+
+const ignoreWarning = {
+  a11y_no_static_element_interactions: (w) => w.message.startsWith("`<sl-"),
+  a11y_click_events_have_key_events: () => true,
 };
