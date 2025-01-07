1b149de43fae7629f611339ca7b1ba6097a666cd
Kasper Ingelvide <kasper@hubben.xyz>
2025-01-07 18:02:42 +0100

    Complete flow

--- web/admin/src/App.svelte
+++ web/admin/src/App.svelte
@@ -1,13 +1,18 @@
 <script lang="ts">
   import Start from "@/pages/Start.svelte";
+  import { ApiClient, OrganizerCredentialsProvider } from "@climblive/lib";
   import configData from "@climblive/lib/config.json";
+  import "@shoelace-style/shoelace/dist/components/button/button.js";
+  import "@shoelace-style/shoelace/dist/components/spinner/spinner.js";
   import { setBasePath } from "@shoelace-style/shoelace/dist/utilities/base-path.js";
   import { QueryClient, QueryClientProvider } from "@tanstack/svelte-query";
   import { SvelteQueryDevtools } from "@tanstack/svelte-query-devtools";
-  import { Route, Router } from "svelte-routing";
-  import Auth from "./pages/Auth.svelte";
+  import { navigate, Route, Router } from "svelte-routing";
+  import { exchangeCode, refreshSession } from "./utils/cognito";
 
   setBasePath("/shoelace");
+
+  let authenticated = $state(false);
 
   const queryClient = new QueryClient({
     defaultOptions: {
@@ -17,22 +22,64 @@
     },
   });
 
+  const authenticate = async () => {
+    const query = new URLSearchParams(location.search);
+    const code = query.get("code");
+
+    if (code != null) {
+      exchangeCode(code).then(({ access_token, refresh_token }) => {
+        ApiClient.getInstance().setCredentialsProvider(
+          new OrganizerCredentialsProvider(access_token),
+        );
+
+        localStorage.setItem("refresh_token", refresh_token);
+
+        authenticated = true;
+
+        navigate("/");
+      });
+    }
+
+    try {
+      const refreshToken = localStorage.getItem("refresh_token");
+
+      if (!refreshToken) {
+        return;
+      }
+
+      if (refreshToken) {
+        const { access_token } = await refreshSession(refreshToken);
+
+        ApiClient.getInstance().setCredentialsProvider(
+          new OrganizerCredentialsProvider(access_token),
+        );
+
+        authenticated = true;
+      }
+    } catch {
+      localStorage.removeItem("refresh_token");
+    }
+  };
+
   const login = () => {
-    const redirectUri = encodeURIComponent(
-      window.location.origin + "/admin/auth",
-    );
+    const redirectUri = encodeURIComponent(window.location.origin + "/admin");
     const url = `https://clmb.auth.eu-west-1.amazoncognito.com/login?response_type=code&client_id=${configData.COGNITO_CLIENT_ID}&redirect_uri=${redirectUri}`;
     window.location.href = url;
   };
 </script>
 
-<QueryClientProvider client={queryClient}>
-  <button onclick={login}>Login</button>
-  <Router>
-    <Route path="/admin"><Start /></Route>
-    <Route path="/admin/auth"><Auth /></Route>
-  </Router>
-  {#if import.meta.env.DEV && false}
-    <SvelteQueryDevtools />
-  {/if}
-</QueryClientProvider>
+{#await authenticate()}
+  <sl-spinner></sl-spinner>
+{:then _}
+  <QueryClientProvider client={queryClient}>
+    {#if !authenticated}
+      <sl-button variant="primary" onclick={login}>Login</sl-button>
+    {/if}
+    <Router basepath="/admin">
+      <Route path="/"><Start /></Route>
+    </Router>
+    {#if import.meta.env.DEV && false}
+      <SvelteQueryDevtools />
+    {/if}
+  </QueryClientProvider>
+{/await}
