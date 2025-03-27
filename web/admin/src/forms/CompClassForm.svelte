ea962368b1adbcc2040cd52cf43d3bdf1f1d32ec
Kasper Ingelvide <kasper@hubben.xyz>
2025-03-27 18:25:01 +0100

    Style revert

--- web/admin/src/forms/CompClassForm.svelte
+++ web/admin/src/forms/CompClassForm.svelte
@@ -60,26 +60,24 @@
       type="text"
       use:value={data.description}
     ></sl-input>
-    <div class="duration">
-      <sl-input
-        size="small"
-        use:name={"timeBegin"}
-        label="Start time"
-        type="datetime-local"
-        use:value={data.timeBegin
-          ? format(data.timeBegin, "yyyy-MM-dd'T'HH:mm")
-          : undefined}
-      ></sl-input>
-      <sl-input
-        size="small"
-        use:name={"timeEnd"}
-        label="End time"
-        type="datetime-local"
-        use:value={data.timeEnd
-          ? format(data.timeEnd, "yyyy-MM-dd'T'HH:mm")
-          : undefined}
-      ></sl-input>
-    </div>
+    <sl-input
+      size="small"
+      use:name={"timeBegin"}
+      label="Start time"
+      type="datetime-local"
+      use:value={data.timeBegin
+        ? format(data.timeBegin, "yyyy-MM-dd'T'HH:mm")
+        : undefined}
+    ></sl-input>
+    <sl-input
+      size="small"
+      use:name={"timeEnd"}
+      label="End time"
+      type="datetime-local"
+      use:value={data.timeEnd
+        ? format(data.timeEnd, "yyyy-MM-dd'T'HH:mm")
+        : undefined}
+    ></sl-input>
     {@render children?.()}
   </fieldset>
 </GenericForm>
@@ -89,11 +87,5 @@
     display: flex;
     flex-direction: column;
     gap: var(--sl-spacing-small);
-  }
-
-  .duration {
-    display: grid;
-    gap: var(--sl-spacing-small);
-    grid-template-columns: repeat(2, 1fr);
   }
 </style>
