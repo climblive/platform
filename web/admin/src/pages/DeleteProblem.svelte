51978b4f2faf0cc81d54f9a66052f5ae4f165c54
Kasper Ingelvide <kasper@hubben.xyz>
2025-05-05 19:21:52 +0200

    Add prompt

--- web/admin/src/pages/DeleteProblem.svelte
+++ web/admin/src/pages/DeleteProblem.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
   import { deleteProblemMutation } from "@climblive/lib/queries";
   import { toastError } from "@climblive/lib/utils";
+  import type { SlDialog } from "@shoelace-style/shoelace";
   import type { Snippet } from "svelte";
 
   type Props = {
     problemID: number;
     children: Snippet<[{ onDelete: () => void }]>;
   };
+
+  let dialog: SlDialog | undefined = $state();
 
   let { problemID, children }: Props = $props();
 
@@ -15,6 +18,10 @@
   const handleDelete = async () => {
     console.log("Deleting problem with ID:", problemID);
 
+    dialog?.show();
+  };
+
+  const confirmDelete = () => {
     $deleteProblem.mutate(undefined, {
       onError: () => toastError("Failed to delete problem."),
     });
@@ -22,3 +29,17 @@
 </script>
 
 {@render children({ onDelete: handleDelete })}
+
+<sl-dialog bind:this={dialog} no-header>
+  <p>
+    <strong>Are you sure?</strong>
+  </p>
+  <p>A problem is deleted permanently and cannot be restored.</p>
+  <sl-button slot="footer" variant="text" onclick={() => dialog?.hide()}
+    >Cancel</sl-button
+  >
+  <sl-button slot="footer" variant="danger" onclick={confirmDelete}
+    >Remove
+    <sl-icon slot="prefix" name="trash"></sl-icon>
+  </sl-button>
+</sl-dialog>
