6eec775bf5951e45bc40e2e1a74ea4bc9dca4615
Kasper Ingelvide <kasper@hubben.xyz>
2025-06-04 19:22:20 +0200

    Format

--- web/packages/lib/src/forms/form.svelte.ts
+++ web/packages/lib/src/forms/form.svelte.ts
@@ -1,36 +1,41 @@
 import type { Attachment } from "svelte/attachments";
 
-export const value = (value: string | number | undefined): Attachment => (
-  node: Element) => {
-  const update = (value: string | number | undefined) => {
-    node.setAttribute("value", value?.toString() ?? "");
+export const value =
+  (value: string | number | undefined): Attachment =>
+  (node: Element) => {
+    const update = (value: string | number | undefined) => {
+      node.setAttribute("value", value?.toString() ?? "");
+    };
+
+    update(value);
+
+    // return {
+    //   update,
+    // };
   };
 
-  update(value);
+export const name =
+  (value: string | number | undefined): Attachment =>
+  (node: Element) => {
+    node.addEventListener("sl-invalid", (e) => e.preventDefault());
 
-  // return {
-  //   update,
-  // };
-};
-
-export const name = (value: string | number | undefined): Attachment => (node: Element) => {
-  node.addEventListener("sl-invalid", (e) => e.preventDefault());
-
-  node.setAttribute("name", value?.toString() ?? "");
-};
-
-export const checked = (value: boolean | undefined): Attachment => (node: Element) => {
-  const update = (value: boolean | undefined) => {
-    if (value) {
-      node.setAttribute("checked", "");
-    } else {
-      node.removeAttribute("checked");
-    }
+    node.setAttribute("name", value?.toString() ?? "");
   };
 
-  update(value);
+export const checked =
+  (value: boolean | undefined): Attachment =>
+  (node: Element) => {
+    const update = (value: boolean | undefined) => {
+      if (value) {
+        node.setAttribute("checked", "");
+      } else {
+        node.removeAttribute("checked");
+      }
+    };
 
-  //  return {
-  //    update,
-  //  };
-};
+    update(value);
+
+    //  return {
+    //    update,
+    //  };
+  };
