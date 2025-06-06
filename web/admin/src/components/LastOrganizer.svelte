f8fe814e0ae14f7cfd205d958a3b9b54afeb68b5
Kasper Ingelvide <kasper@hubben.xyz>
2025-06-06 14:45:25 +0200

    feat: auto select last organizer

---
+++ web/admin/src/components/LastOrganizer.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import { navigate } from "svelte-routing";
+
+  onMount(() => {
+    const organizerId = localStorage.getItem("organizerId");
+    if (!organizerId) {
+      return;
+    }
+
+    navigate(`/admin/organizers/${organizerId}`, {
+      replace: true,
+    });
+  });
+</script>
---
+++ web/admin/src/pages/OrganizerView.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import "@shoelace-style/shoelace/dist/components/button/button.js";
+  import { onMount } from "svelte";
+  import ContestList from "./ContestList.svelte";
+
+  interface Props {
+    organizerId: number;
+  }
+
+  let { organizerId }: Props = $props();
+
+  onMount(() => {
+    localStorage.setItem("organizerId", String(organizerId));
+  });
+</script>
+
+<ContestList organizerId={Number(organizerId)} />
