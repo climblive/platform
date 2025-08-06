b142af524f3a37bdbe891319a3bbd62a42a02c2f
Kasper Ingelvide <kasper@hubben.xyz>
2025-08-06 19:32:01 +0200

    Use shadow token

--- web/scorecard/src/components/TickBox.svelte
+++ web/scorecard/src/components/TickBox.svelte
@@ -219,9 +219,7 @@
 
   wa-popup::part(popup) {
     background-color: var(--wa-color-brand-fill-loud);
-    box-shadow:
-      rgba(50, 50, 93, 0.25) 0px 13px 27px -5px,
-      rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
+    box-shadow: var(--wa-shadow-l);
     border-radius: var(--wa-border-radius-m);
     padding: var(--wa-space-s);
     cursor: default;
