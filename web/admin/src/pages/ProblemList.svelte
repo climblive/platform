423abb1fa540818dd82575771ea8b57af72670fa
Kasper Ingelvide <kasper@hubben.xyz>
2025-08-09 10:25:13 +0200

    Fix problems

--- web/admin/src/pages/ProblemList.svelte
+++ web/admin/src/pages/ProblemList.svelte
@@ -2,9 +2,9 @@
   import {
     HoldColorIndicator,
     Table,
-    TableCell,
-    TableRow,
+    type ColumnDefinition,
   } from "@climblive/lib/components";
+  import type { Problem } from "@climblive/lib/models";
   import { getProblemsQuery } from "@climblive/lib/queries";
   import { navigate } from "svelte-routing";
   import DeleteProblem from "./DeleteProblem.svelte";
@@ -18,50 +18,72 @@
   const problemsQuery = $derived(getProblemsQuery(contestId));
 
   let problems = $derived($problemsQuery.data);
+
+  const columns: ColumnDefinition<Problem>[] = [
+    {
+      label: "Number",
+      mobile: true,
+      render: renderNumberAndColor,
+    },
+    {
+      label: "Points",
+      mobile: true,
+      render: renderPoints,
+    },
+    {
+      mobile: true,
+      render: renderControls,
+      align: "right",
+    },
+  ];
 </script>
 
+{#snippet renderNumberAndColor({
+  number,
+  holdColorPrimary,
+  holdColorSecondary,
+}: Problem)}
+  <HoldColorIndicator
+    --height="1.25rem"
+    --width="1.25rem"
+    primary={holdColorPrimary}
+    secondary={holdColorSecondary}
+  />
+  № {number}
+{/snippet}
+
+{#snippet renderPoints({ pointsTop }: Problem)}
+  {pointsTop}
+{/snippet}
+
+{#snippet renderControls({ id }: Problem)}
+  <wa-button
+    size="small"
+    appearance="plain"
+    onclick={() => navigate(`/admin/problems/${id}/edit`)}
+    label="Edit"
+  >
+    <wa-icon name="pencil"></wa-icon>
+  </wa-button>
+  <DeleteProblem problemId={id}>
+    {#snippet children({ deleteProblem })}
+      <wa-button
+        size="small"
+        variant="danger"
+        appearance="plain"
+        onclick={deleteProblem}
+        label={`Delete problem ${id}`}
+      >
+        <wa-icon name="trash"></wa-icon>
+      </wa-button>
+    {/snippet}
+  </DeleteProblem>
+{/snippet}
+
 <section>
-  <Table columns={["Number", "Color", "Points", ""]}>
-    {#if problems}
-      {#each problems as problem (problem.id)}
-        <TableRow>
-          <TableCell>№ {problem.number}</TableCell>
-          <TableCell
-            ><HoldColorIndicator
-              --height="1.25rem"
-              --width="1.25rem"
-              primary={problem.holdColorPrimary}
-              secondary={problem.holdColorSecondary}
-            /></TableCell
-          >
-          <TableCell>{problem.pointsTop}</TableCell>
-          <TableCell align="right">
-            <wa-button
-              size="small"
-              appearance="plain"
-              onclick={() => navigate(`/admin/problems/${problem.id}/edit`)}
-              label="Edit"
-            >
-              <wa-icon name="pencil"></wa-icon>
-            </wa-button>
-            <DeleteProblem problemId={problem.id}>
-              {#snippet children({ deleteProblem })}
-                <wa-button
-                  size="small"
-                  variant="danger"
-                  appearance="plain"
-                  onclick={deleteProblem}
-                  label={`Delete problem ${problem.id}`}
-                >
-                  <wa-icon name="trash"></wa-icon>
-                </wa-button>
-              {/snippet}
-            </DeleteProblem>
-          </TableCell>
-        </TableRow>
-      {/each}
-    {/if}
-  </Table>
+  {#if problems}
+    <Table {columns} data={problems} getId={({ id }) => id}></Table>
+  {/if}
 </section>
 
 <style>
