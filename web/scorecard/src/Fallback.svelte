d84de4624e113526bb8c95e741aaf1f271b13d1a
Kasper Ingelvide <kasper@hubben.xyz>
2025-08-27 18:04:59 +0200

    Try native

--- web/scorecard/src/Fallback.svelte
+++ web/scorecard/src/Fallback.svelte
@@ -1,1 +1,25 @@
-<p>Your browser is too old.</p>
+<script lang="ts">
+  import "@awesome.me/webawesome/dist/styles/native.css";
+</script>
+
+<main>
+  <section>
+    <p>Your browser is too old.</p>
+    <button>Failsafe</button>
+  </section>
+</main>
+
+<style>
+  main {
+    width: 100%;
+  }
+
+  section {
+    margin: var(--wa-space-m);
+    padding: var(--wa-space-m);
+    background-color: var(--wa-surface-raised);
+    border: var(--wa-border-width-s) var(--wa-border-style)
+      var(--wa-color-neutral-border-normal);
+    border-radius: var(--wa-border-radius-m);
+  }
+</style>
