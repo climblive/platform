71e9a4797fa4241316f2e072bf70fad3ae49fc0e
Kasper Ingelvide <7457770+kasper-i@users.noreply.github.com>
2025-08-31 20:11:31 +0200

    chore: add sentry (#480)

--- web/scorecard/src/main.ts
+++ web/scorecard/src/main.ts
@@ -6,9 +6,18 @@ import {
   updateTheme,
   watchColorSchemeChanges,
 } from "@climblive/lib/utils";
+import * as Sentry from "@sentry/svelte";
 import { mount } from "svelte";
 import App from "./App.svelte";
 import NativeStyles from "./NativeStyles.svelte";
+
+if (import.meta.env.PROD) {
+  Sentry.init({
+    dsn: "https://019099d850441f60cea5d465e217f768@o4509937603641344.ingest.de.sentry.io/4509937616093264",
+    sendDefaultPii: false,
+    environment: import.meta.env.VITE_SENTRY_ENVIRONMENT,
+  });
+}
 
 watchColorSchemeChanges((prefersDarkColorScheme) =>
   updateTheme(prefersDarkColorScheme),
