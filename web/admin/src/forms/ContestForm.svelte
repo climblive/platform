33184231439b0523502e0eb102da28e4e424c1f7
Kasper Ingelvide <kasper@hubben.xyz>
2025-09-18 20:28:54 +0200

    feat(admin): rich text rules editor

--- web/admin/src/forms/ContestForm.svelte
+++ web/admin/src/forms/ContestForm.svelte
@@ -16,11 +16,16 @@
 </script>
 
 <script lang="ts">
+  import "@awesome.me/webawesome/dist/components/button-group/button-group.js";
+  import "@awesome.me/webawesome/dist/components/button/button.js";
   import "@awesome.me/webawesome/dist/components/input/input.js";
   import "@awesome.me/webawesome/dist/components/textarea/textarea.js";
+  import type WaTextarea from "@awesome.me/webawesome/dist/components/textarea/textarea.js";
   import { GenericForm, name } from "@climblive/lib/forms";
   import type { Contest } from "@climblive/lib/models";
-  import { type Snippet } from "svelte";
+  import { Editor } from "@tiptap/core";
+  import { StarterKit } from "@tiptap/starter-kit";
+  import { onDestroy, onMount, type Snippet } from "svelte";
 
   type T = $$Generic<Partial<Contest>>;
 
@@ -32,6 +37,27 @@
   }
 
   let { data, schema, submit, children }: Props = $props();
+
+  let element: HTMLElement | undefined = $state();
+  let rules: WaTextarea | undefined = $state();
+  let editor = $state<Editor>();
+
+  onMount(() => {
+    editor = new Editor({
+      element: element,
+      extensions: [StarterKit],
+      content: data.rules,
+
+      onUpdate: ({ editor }) => {
+        if (rules) {
+          rules.value = editor.getHTML();
+        }
+      },
+    });
+  });
+  onDestroy(() => {
+    editor?.destroy();
+  });
 </script>
 
 <GenericForm {schema} {submit}>
@@ -92,12 +118,80 @@
       value={Math.floor((data.gracePeriod ?? 0) / minuteInNanoseconds)}
     >
     </wa-input>
-    <wa-textarea
-      size="small"
-      {@attach name("rules")}
-      label="Rules"
-      value={data.rules ?? ""}
-    ></wa-textarea>
+    <div>
+      <wa-textarea
+        size="small"
+        {@attach name("rules")}
+        label="Rules"
+        value={data.rules ?? ""}
+        bind:this={rules}
+      ></wa-textarea>
+      {#if editor}
+        <wa-button-group>
+          <wa-button
+            size="small"
+            onclick={(e: MouseEvent) => {
+              e.preventDefault();
+              editor?.chain().focus().toggleHeading({ level: 1 }).run();
+            }}
+            class:active={editor.isActive("heading", { level: 1 })}
+          >
+            <wa-icon name="heading"></wa-icon>
+          </wa-button>
+          <wa-button
+            size="small"
+            onclick={(e: MouseEvent) => {
+              e.preventDefault();
+              editor?.chain().focus().setParagraph().run();
+            }}
+            class:active={editor.isActive("paragraph")}
+          >
+            <wa-icon name="paragraph"></wa-icon>
+          </wa-button>
+          <wa-button
+            size="small"
+            onclick={(e: MouseEvent) => {
+              e.preventDefault();
+              editor?.chain().focus().setItalic().run();
+            }}
+            class:active={editor.isActive("paragraph")}
+          >
+            <wa-icon name="italic"></wa-icon>
+          </wa-button>
+          <wa-button
+            size="small"
+            onclick={(e: MouseEvent) => {
+              e.preventDefault();
+              editor?.chain().focus().setBold().run();
+            }}
+            class:active={editor.isActive("paragraph")}
+          >
+            <wa-icon name="bold"></wa-icon>
+          </wa-button>
+          <wa-button
+            size="small"
+            onclick={(e: MouseEvent) => {
+              e.preventDefault();
+              editor?.chain().focus().setUnderline().run();
+            }}
+            class:active={editor.isActive("paragraph")}
+          >
+            <wa-icon name="underline"></wa-icon>
+          </wa-button>
+          <wa-button
+            size="small"
+            onclick={(e: MouseEvent) => {
+              e.preventDefault();
+              editor?.chain().focus().setStrike().run();
+            }}
+            class:active={editor.isActive("paragraph")}
+          >
+            <wa-icon name="strikethrough"></wa-icon>
+          </wa-button>
+        </wa-button-group>
+      {/if}
+      <div bind:this={element} class="rules"></div>
+    </div>
     {@render children?.()}
   </fieldset>
 </GenericForm>
@@ -107,5 +201,21 @@
     display: flex;
     flex-direction: column;
     gap: var(--wa-space-s);
+  }
+
+  .rules :global(.tiptap) {
+    min-height: 10rem;
+    border: var(--wa-form-control-border-style)
+      var(--wa-form-control-border-width) var(--wa-form-control-border-color);
+    border-radius: var(--wa-form-control-border-radius);
+    padding: 0 var(--wa-form-control-padding-inline);
+  }
+
+  wa-textarea::part(base) {
+    display: none;
+  }
+
+  wa-button-group {
+    margin-block-end: var(--wa-space-xs);
   }
 </style>
