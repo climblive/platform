03bfcd20e18abfdce1b4d794dae845950ffdb955
Kasper Ingelvide <kasper@hubben.xyz>
2025-09-21 12:01:00 +0200

    Fix

--- backend/internal/usecases/mocks_test.go
+++ backend/internal/usecases/mocks_test.go
@@ -88,6 +88,11 @@ func (m *repositoryMock) GetContest(ctx context.Context, tx domain.Transaction, 
 	return args.Get(0).(domain.Contest), args.Error(1)
 }
 
+func (m *repositoryMock) GetAllContests(ctx context.Context, tx domain.Transaction) ([]domain.Contest, error) {
+	args := m.Called(ctx, tx)
+	return args.Get(0).([]domain.Contest), args.Error(1)
+}
+
 func (m *repositoryMock) StoreContest(ctx context.Context, tx domain.Transaction, contest domain.Contest) (domain.Contest, error) {
 	args := m.Called(ctx, tx, contest)
 
