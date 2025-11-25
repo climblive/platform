d2b772dfd6428f712431d11ac1cd0d34aeb0ce61
Kasper Ingelvide <7457770+kasper-i@users.noreply.github.com>
2025-11-25 23:24:54 +0100

    feat(admin): help page (#568)

--- web/admin/src/Main.svelte
+++ web/admin/src/Main.svelte
@@ -10,6 +10,7 @@
   import EditCompClass from "./pages/EditCompClass.svelte";
   import EditContest from "./pages/EditContest.svelte";
   import EditProblem from "./pages/EditProblem.svelte";
+  import Help from "./pages/Help.svelte";
   import OrganizerView from "./pages/OrganizerView.svelte";
   import PrintableTicketList from "./pages/PrintableTicketList.svelte";
   import RaffleView from "./pages/RaffleView.svelte";
@@ -22,6 +23,9 @@
   <Router basepath="/admin">
     <Route path="/">
       <Root />
+    </Route>
+    <Route path="/help">
+      <Help />
     </Route>
     <Route path="/organizers/:organizerId">
       {#snippet children({ params }: { params: { organizerId: number } })}
