9ee9cccf828db03512d4d4187d9e37a3e2d4844e
Kasper Ingelvide <kasper@hubben.xyz>
2025-12-30 11:44:40 +0100

    Fix again

--- backend/cmd/api/migrations/00032_add_contest_created.sql
+++ backend/cmd/api/migrations/00032_add_contest_created.sql
@@ -1,5 +1,5 @@
 -- +goose Up
-ALTER TABLE `contest` ADD COLUMN `created` TIMESTAMP NOT NULL DEFAULT '1970-01-01 00:00:00' AFTER `grace_period`;
+ALTER TABLE `contest` ADD COLUMN `created` TIMESTAMP NOT NULL DEFAULT '1970-01-01 00:00:01' AFTER `grace_period`;
 
 -- +goose Down
 ALTER TABLE `contest` DROP COLUMN `created`;
