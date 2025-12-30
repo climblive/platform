bf381805744279523b27935596eda6c9a8c681bc
Kasper Ingelvide <kasper@hubben.xyz>
2025-12-30 09:28:18 +0100

    Improve

--- backend/internal/usecases/organizer_ucase_test.go
+++ backend/internal/usecases/organizer_ucase_test.go
@@ -14,6 +14,8 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+const whitespaceCharacters = "\u0009\u000A\u000B\u000C\u000D\u0020\u0085\u00A0\u1680\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u2028\u2029\u202F\u205F\u3000"
+
 func TestCreateOrganizer(t *testing.T) {
 	fakedUsername := "alice"
 	fakedUserID := randomResourceID[domain.UserID]()
@@ -84,7 +86,9 @@ func TestCreateOrganizer(t *testing.T) {
 			Repo: mockedRepo,
 		}
 
-		organizer, err := ucase.CreateOrganizer(context.Background(), domain.OrganizerTemplate{})
+		organizer, err := ucase.CreateOrganizer(context.Background(), domain.OrganizerTemplate{
+			Name: whitespaceCharacters,
+		})
 
 		assert.ErrorIs(t, err, domain.ErrInvalidData)
 		assert.Equal(t, domain.Organizer{}, organizer)
@@ -530,10 +534,8 @@ func TestPatchOrganizer(t *testing.T) {
 			Authorizer: mockedAuthorizer,
 		}
 
-		whitspace := "\u0009\u000A\u000B\u000C\u000D\u0020\u00A0\u1680\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u3000"
-
 		patch := domain.OrganizerPatch{
-			Name: domain.NewPatch(whitspace),
+			Name: domain.NewPatch(whitespaceCharacters),
 		}
 
 		organizer, err := ucase.PatchOrganizer(context.Background(), fakedOrganizerID, patch)
