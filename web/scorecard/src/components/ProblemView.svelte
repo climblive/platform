d85a774fbb8683e33ead66d369cf36c724a03098
Kasper Ingelvide <kasper@hubben.xyz>
2026-01-26 19:42:01 +0100

    Fix

--- web/scorecard/src/components/ProblemView.svelte
+++ web/scorecard/src/components/ProblemView.svelte
@@ -31,7 +31,7 @@
   />
   <span class="points">
     <span class="top">
-      {problem.pointsTop} pts
+      {problem.pointsTop}p
     </span>
     {#if problem.flashBonus}
       <wa-icon name="bolt"></wa-icon>
