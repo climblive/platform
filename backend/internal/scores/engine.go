d30726eb3ccb42213a0af0ecd70e8d09aa5005a2
Kasper Ingelvide <kasper@hubben.xyz>
2026-02-06 17:34:42 +0100

    Fix lint

--- backend/internal/scores/engine.go
+++ backend/internal/scores/engine.go
@@ -378,10 +378,10 @@ func (e *DefaultScoreEngine) CalculateProblemValue(compClassID domain.CompClassI
 			CompClassID: compClassID,
 
 			ProblemValue: domain.ProblemValue{
-				PointsZone1: problem.ProblemValue.PointsZone1 / max(1, numZone1),
-				PointsZone2: problem.ProblemValue.PointsZone2 / max(1, numZone2),
-				PointsTop:   problem.ProblemValue.PointsTop / max(1, numTop),
-				FlashBonus:  problem.ProblemValue.FlashBonus / max(1, numFlash),
+				PointsZone1: problem.PointsZone1 / max(1, numZone1),
+				PointsZone2: problem.PointsZone2 / max(1, numZone2),
+				PointsTop:   problem.PointsTop / max(1, numTop),
+				FlashBonus:  problem.FlashBonus / max(1, numFlash),
 			},
 		}
 	}
