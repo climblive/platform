877a620e5637e5db8da3cc912af9bdb45a18190a
Kasper Ingelvide <kasper@hubben.xyz>
2026-02-18 20:11:29 +0100

    Change index

--- backend/cmd/api/migrations/00035_add_scrub.sql
+++ backend/cmd/api/migrations/00035_add_scrub.sql
@@ -3,8 +3,7 @@ ALTER TABLE `contest` ADD COLUMN `name_retention_time` INT NOT NULL DEFAULT 6048
 ALTER TABLE `contender` ADD COLUMN `scrub_before` TIMESTAMP NULL DEFAULT NULL;
 ALTER TABLE `contender` ADD COLUMN `scrubbed_at` TIMESTAMP NULL DEFAULT NULL;
 
-CREATE INDEX `index6` ON `contender` (`scrubbed_at` ASC);
-CREATE INDEX `index7` ON `contender` (`scrub_before` ASC);
+CREATE INDEX `index6` ON `contender` (`scrubbed_at` ASC, `scrub_before` ASC, `name` ASC);
 
 UPDATE contender c
 JOIN comp_class cc ON c.class_id = cc.id
@@ -13,7 +12,6 @@ SET c.scrub_before = DATE_ADD(cc.time_end, INTERVAL ct.name_retention_time SECON
 WHERE c.class_id IS NOT NULL AND c.entered IS NOT NULL;
 
 -- +goose Down
-DROP INDEX `index7` ON `contender`;
 DROP INDEX `index6` ON `contender`;
 
 ALTER TABLE `contender` DROP COLUMN `scrubbed_at`;
