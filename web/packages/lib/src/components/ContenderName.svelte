2bb5128ba5c24e2f486c2c6ab391924c306d8520
Kasper Ingelvide <kasper@hubben.xyz>
2026-02-26 18:36:54 +0100

    Fix

--- web/packages/lib/src/components/ContenderName.svelte
+++ web/packages/lib/src/components/ContenderName.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import "@awesome.me/webawesome/dist/components/tooltip/tooltip.js";
+  import "@awesome.me/webawesome/dist/components/popover/popover.js";
   import { format } from "date-fns";
   import { maskScrubbedName } from "../utils";
 
@@ -19,9 +19,30 @@
 </script>
 
 {#if scrubbedAt}
-  <wa-tooltip for={tooltipId}
-    >Anonymized on {format(scrubbedAt, "yyyy-MM-dd HH:mm")}</wa-tooltip
-  >
+  <wa-popover for={tooltipId}>
+    The name of the contender was removed and anonymized on {format(
+      scrubbedAt,
+      "yyyy-MM-dd HH:mm",
+    )}.
+
+    <wa-button data-popover="close" variant="primary" size="small"
+      >Got it!</wa-button
+    >
+  </wa-popover>
 {/if}
 
 <span id={tooltipId}>{displayName}</span>
+
+<style>
+  wa-popover {
+    --max-width: 300px;
+  }
+
+  wa-popover::part(body) {
+    margin-inline-start: var(--wa-space-l);
+  }
+
+  wa-button {
+    margin-block-start: var(--wa-space-m);
+  }
+</style>
