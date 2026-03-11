f9cb5c3b99e5744a640a55e6e319cdad203289f0
Kasper Ingelvide <kasper@hubben.xyz>
2026-03-11 20:21:53 +0100

    Revert

--- backend/internal/database/queries.sql.go
+++ backend/internal/database/queries.sql.go
@@ -936,15 +936,22 @@ func (q *Queries) GetRafflesByContest(ctx context.Context, contestID int32) ([]G
 }
 
 const getScrubEligibleContenders = `-- name: GetScrubEligibleContenders :many
-SELECT contender.id, contender.organizer_id, contender.contest_id, contender.registration_code, contender.name, contender.class_id, contender.entered, contender.disqualified, contender.withdrawn_from_finals, contender.scrubbed_at, contender.scrub_before
+SELECT contender.id, contender.organizer_id, contender.contest_id, contender.registration_code, contender.name, contender.class_id, contender.entered, contender.disqualified, contender.withdrawn_from_finals, contender.scrubbed_at, contender.scrub_before, score.contender_id, score.timestamp, score.score, score.placement, score.finalist, score.rank_order
 FROM contender
+LEFT JOIN score ON score.contender_id = id
 WHERE contender.name != ''
   AND contender.scrub_before IS NOT NULL
   AND contender.scrub_before < ?
 `
 
 type GetScrubEligibleContendersRow struct {
-	Contender Contender
+	Contender   Contender
+	ContenderID sql.NullInt32
+	Timestamp   sql.NullTime
+	Score       sql.NullInt32
+	Placement   sql.NullInt32
+	Finalist    sql.NullBool
+	RankOrder   sql.NullInt32
 }
 
 func (q *Queries) GetScrubEligibleContenders(ctx context.Context, scrubBefore sql.NullTime) ([]GetScrubEligibleContendersRow, error) {
@@ -968,6 +975,12 @@ func (q *Queries) GetScrubEligibleContenders(ctx context.Context, scrubBefore sq
 			&i.Contender.WithdrawnFromFinals,
 			&i.Contender.ScrubbedAt,
 			&i.Contender.ScrubBefore,
+			&i.ContenderID,
+			&i.Timestamp,
+			&i.Score,
+			&i.Placement,
+			&i.Finalist,
+			&i.RankOrder,
 		); err != nil {
 			return nil, err
 		}
