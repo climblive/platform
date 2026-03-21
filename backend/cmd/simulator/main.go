b24c586b20c3bd7eef4654afb6969e34f353bdb5
Kasper Ingelvide <kasper@hubben.xyz>
2026-03-21 12:08:39 +0100

    Fix

--- backend/cmd/simulator/main.go
+++ backend/cmd/simulator/main.go
@@ -47,7 +47,9 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to open config file: %v", err)
 	}
-	defer f.Close()
+	defer func() {
+		_ = f.Close()
+	}()
 
 	if err := json.NewDecoder(f).Decode(&cfg); err != nil {
 		log.Fatalf("failed to parse config file: %v", err)
