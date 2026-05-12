8b1dac732ce40e9c9731397efee6ddc12df05ec2
Kasper Ingelvide <kasper@hubben.xyz>
2026-05-12 18:10:12 +0200

    Revert zod

--- web/scorecard/src/forms/RegistrationForm.svelte
+++ web/scorecard/src/forms/RegistrationForm.svelte
@@ -16,7 +16,7 @@
   const registrationFormSchema: z.ZodType<ContenderPatch> = z.object({
     name: z.string().min(1),
     compClassId: z.coerce.number().gt(0, { message: "" }),
-    withdrawnFromFinals: z.coerce.boolean().optional(),
+    withdrawnFromFinals: z.coerce.boolean(),
   });
 
   interface Props {
