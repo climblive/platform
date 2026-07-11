44c3cf44057d2cef1a4781b9759af33b2208b37a
Kasper Ingelvide <kasper@hubben.xyz>
2026-07-11 14:02:29 +0200

    Fix

--- backend/internal/scores/ranker.go
+++ backend/internal/scores/ranker.go
@@ -76,7 +76,7 @@ func (r *BasicRanker) RankContenders(contenders iter.Seq[Contender]) []domain.Sc
 		if r.usePoints {
 			scoreValue = fmt.Sprintf("%dp", contender.Points)
 		} else {
-			scoreValue = fmt.Sprintf("%dt %dz₂ %dz₁ %d %d %d", contender.Tops, contender.Zone2s, contender.Zone1s, contender.AttemptsTops, contender.AttemptsZone2s, contender.AttemptsZone1s)
+			scoreValue = fmt.Sprintf("%dt %dz₂ %dz₁", contender.Tops, contender.Zone2s, contender.Zone1s)
 		}
 
 		score := domain.Score{
