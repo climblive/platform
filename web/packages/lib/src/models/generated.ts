5afbb08b1834ab64d7a8832832cb30ca478823b1
Kasper Ingelvide <kasper@hubben.xyz>
2026-08-06 13:01:37 +0200

    Fmt

--- web/packages/lib/src/models/generated.ts
+++ web/packages/lib/src/models/generated.ts
@@ -3,7 +3,7 @@
 //////////
 // source: id.go
 
-export type ResourceID = number /* int32 */;
+export type ResourceID = number; /* int32 */
 export type CompClassID = ResourceID;
 export type ContenderID = ResourceID;
 export type ContestID = ResourceID;
@@ -15,8 +15,17 @@ export type SeriesID = ResourceID;
 export type UserID = ResourceID;
 export type TickID = ResourceID;
 export type OrganizerInviteID = string;
-export type ResourceIDType = 
-    CompClassID | ContenderID | ContestID | OrganizerID | ProblemID | RaffleID | RaffleWinnerID | SeriesID | UserID | TickID;
+export type ResourceIDType =
+  | CompClassID
+  | ContenderID
+  | ContestID
+  | OrganizerID
+  | ProblemID
+  | RaffleID
+  | RaffleWinnerID
+  | SeriesID
+  | UserID
+  | TickID;
 export type ScoreEngineInstanceID = string;
 
 //////////
