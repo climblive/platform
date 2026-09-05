fce37d798040fe2b187f87dc5a6a3f922b381ece
Kasper Ingelvide <kasper@hubben.xyz>
2026-09-05 22:40:25 +0200

    Change tag

--- web/admin/src/components/RulesEditor.svelte
+++ web/admin/src/components/RulesEditor.svelte
@@ -58,7 +58,7 @@
   <RuleOptionCard
     title="Attempts"
     description="Competitors are ranked based on the number of attempts needed to complete problems."
-    tag="Early access"
+    tag="Beta"
   >
     {#snippet header()}
       <wa-radio
