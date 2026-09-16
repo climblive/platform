3cfd1aeb83e902f431e2a0872409124ec4c8641b
Kasper Ingelvide <kasper@hubben.xyz>
2026-09-16 21:22:18 +0200

    fix(scorecard): auto close results tab when document is hidden

--- web/scorecard/src/pages/Scorecard.svelte
+++ web/scorecard/src/pages/Scorecard.svelte
@@ -354,7 +354,7 @@
 
   const tearDown = () => {
     resultsConnected = false;
-    tabGroup?.setAttribute("active", "problems");
+    tabGroup?.setAttribute("active", "scorecard");
 
     eventSource?.close();
     eventSource = undefined;
