ee7627cd5a22970e6adab94244a55d647fcccd0c
Kasper Ingelvide <kasper@hubben.xyz>
2026-09-21 19:23:26 +0200

    Fixes

--- web/admin/src/pages/RestoreContest.svelte
+++ web/admin/src/pages/RestoreContest.svelte
@@ -4,13 +4,15 @@
   import "@awesome.me/webawesome/dist/components/icon/icon.js";
   import { restoreContestMutation } from "@climblive/lib/queries";
   import { toastUnexpectedError } from "@climblive/lib/utils";
+  import type { Snippet } from "svelte";
   import { navigate } from "svelte-routing";
 
   type Props = {
     contestId: number;
+    children?: Snippet<[{ restoreContest: () => void }]>;
   };
 
-  let { contestId }: Props = $props();
+  let { contestId, children }: Props = $props();
 
   const restoreContest = $derived(restoreContestMutation(contestId));
 
@@ -26,21 +28,28 @@
   };
 </script>
 
-<section>
-  <wa-callout variant="danger" size="s">
-    <wa-icon slot="icon" name="box-archive"></wa-icon>
-    <p>
-      <strong>This competition has been archived</strong><br />
-      You can restore this competition at any time to make it active again.
-    </p>
-  </wa-callout>
+{#if children}
+  {@render children({
+    restoreContest: handleRestore,
+  })}
+{:else}
+  <section>
+    <wa-callout variant="danger" size="s">
+      <wa-icon slot="icon" name="box-archive"></wa-icon>
+      <p>
+        <strong>This competition has been archived</strong><br />
+        You can restore this competition at any time to make it active again.
+      </p>
+    </wa-callout>
 
-  <wa-button
-    onclick={handleRestore}
-    appearance="filled-outlined"
-    variant="success">Restore</wa-button
-  >
-</section>
+    <wa-button
+      onclick={handleRestore}
+      loading={restoreContest.isPending}
+      appearance="filled-outlined"
+      variant="success">Restore</wa-button
+    >
+  </section>
+{/if}
 
 <style>
   section {
